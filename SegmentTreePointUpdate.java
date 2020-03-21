ee93ec61e3fea766f4e46faac9775eff469544fc
RishabhSharma333 <39693699+RishabhSharma333@users.noreply.github.com>
2020-03-21 21:28:34 +0530

    Update SegmentTreePointUpdate.java

--- SegmentTreePointUpdate.java
+++ SegmentTreePointUpdate.java
@@ -6,7 +6,111 @@ public class SegmentTree {//It is implemented as a array heap like in heapsort
     //in this implementation Rmq returns index of min value and not the value
     //another way is to built the n^2 array in n^2 time and give query in o(1) time
     //this implementation of range minimum Query(note ) works for negative values also leaving the Integer.MAX_value
+    public static void main(String[] args) {import java.util.Arrays;
+
+public class SegmentTree {//It is implemented as a array heap like in heapsort
+    //in this implementation Rmq returns index of min value and not the value
+    //another way is to built the n^2 array in n^2 time and give query in o(1) time
+    //this implementation of range minimum Query(note ) works for negative values also leaving the Integer.MAX_value
     public static void main(String[] args) {
+       int arr[]={18, 17, 13, 19,15,99,16,8,9,10};
+        int x = (int) (Math.ceil(Math.log(arr.length) / Math.log(2)));//height of segment tree
+        int max_size = 2 * (int) Math.pow(2, x) - 1;
+       int ret[]=new int[max_size];
+       built(arr,ret,0,0,6);
+        System.out.println(rMin(4,6,arr,ret));//here rMinQ(3,6) returns 15 means 3, 6 both included
+        updateRmq(arr,ret,5,0);
+        updateRmq(arr,ret,4,1);
+        updateRmq(arr,ret,40,2);
+        updateRmq(arr,ret,100,3);
+        updateRmq(arr,ret,23,4);
+        updateRmq(arr,ret,32,5);
+        updateRmq(arr,ret,1,6);
+        updateRmq(arr,ret,40,2);
+    }
+
+    static int left(int t){
+        return (t<<1)+1;
+    }
+
+    static int right(int t){
+        return (t<<1) +2;
+    }
+
+    static int sibling(int p){
+        return p%2==0?p-1:p+1;
+
+    }
+
+    static int getParent(int n){
+        return (n-n/2)-1;
+    }
+
+    static void built(int arr[],int ret[],int pi,int l,int r)//pi is present index pi,l,r go along always
+    {
+        if(l==r){ret[pi]=l;
+        }
+        else{
+            built(arr,ret,left(pi),l,(r+l)/2);
+            built(arr,ret,right(pi),((r+l)/2)+1,r);
+            int pp=ret[left(pi)];
+            int ppp=ret[right(pi)];
+            int ans=Math.min(arr[pp],arr[ppp])==arr[pp]?pp:ppp;
+            ret[pi]=ans;
+        }
+    }
+
+    static int rMin(int i,int j,int pi,int l,int r,int arr[],int ret[]){//use the overloaded method
+        if(i>r||j<l)return -1;
+        if (l >= i && r <= j) return ret[pi];
+        else {
+            int pp=rMin(i,j,left(pi),l,(r+l)/2,arr,ret);
+            int ppp=rMin(i,j,right(pi),(r+l)/2+1,r,arr,ret);
+            if(pp==-1)return ppp;
+            if(ppp==-1) return pp;
+            return (arr[pp] <= arr[ppp]) ? pp : ppp;//returns prevoius element in case of same elements
+        }
+    }
+
+    static int rMin(int i,int j,int arr[],int ret[]){
+        return rMin(i,j,0,0,arr.length-1,arr,ret);
+    }
+
+    static void updateRmq(int arr[],int ret[],int val,int ind,int pi,int l,int r){
+        arr[ind]=val;
+
+        while(l!=r){
+            int ll=(l+r)/2;
+            int rr=ll+1;
+          if(ind>=l&&ind<=ll){r=ll;pi=left(pi);}
+          else {l=rr;pi=right(pi);}
+        }
+        ret[pi]=ind;
+        System.out.println(pi+" pi");
+        System.out.println("ret"+Arrays.toString(ret));
+        while(pi!=0){
+            int si=sibling(pi);
+            int p=getParent(pi);
+            if(arr[ret[si]]<arr[ret[pi]]){
+                ret[p]=ret[si];
+                System.out.println(ret[p]+"  "+ si);
+            }
+            else{
+                ret[p]=ret[pi];
+            }
+            pi=p;
+            System.out.println(pi);
+        }
+        System.out.println("arr "+Arrays.toString(arr));
+        System.out.println("ret"+Arrays.toString(ret));
+    }
+
+    static  void updateRmq(int arr[],int ret[],int val,int index){
+        updateRmq(arr,ret,val,index,0,0,arr.length-1);
+    }
+
+}
+
        int arr[]={18, 17, 13, 19,15,99,16};
        int ret[]=new int[arr.length*2-1];
        built(arr,ret,0,0,6);
